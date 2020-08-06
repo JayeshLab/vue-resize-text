69e19c34faa4dbb1a0b8c4748b379ddb6055573e
JayeshLab <helloiamjayesh@gmail.com>
2020-08-06 17:14:16 +0530

    update test plugin

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,6 @@ module.exports = {
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  testURL: 'http://localhost/'
-
+  testURL: 'http://localhost/',
+  preset: '@vue/cli-plugin-unit-jest'
 }
